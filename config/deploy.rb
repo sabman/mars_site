3797101b3ab8e5bf8ed69b0502bdbcb78f87aea3
Shoaib Burq <sburq@rhe-seismic.(none)>
2010-04-29 11:49:26 +1000

    update the googlemaps API key for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,7 @@ namespace :deploy do
   desc "Symlink shared configs and folders on each release"
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/ldap_root_user.yml #{release_path}/config/ldap_root_user.yml"
     run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
   end
 
